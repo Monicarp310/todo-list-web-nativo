9210adf1cafb89f7c8a1024236b52a7934e97ea0
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-02-28 10:44:25 +0100

    first commit

---
+++ to-do-main/script.js
@@ -0,0 +1,50 @@
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el))
+}
+
+const vaciarLista = () =>{
+    
+}
+
+<script>
+function limpia(elemento)
+{
+elemento.value = "";
+}
+
+function verifica(elemento)
+{
+if(elemento.value = "")
+elemento.value = "Default Value";
+}
+</script>
+
+setDate();
