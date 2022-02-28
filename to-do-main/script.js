5750d615641ba2282d72a3b954cd0ed2cb0ab707
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-02-28 11:28:05 +0100

    MAQUETACIÓN

--- to-do-main/script.js
+++ to-do-main/script.js
@@ -1,50 +1,37 @@
 // Tasks Container
-const tasksContainer = document.getElementById('tasksContainer');
+const tasksContainer = document.getElementById("tasksContainer");
 
-const addNewTask = event => {
-    event.preventDefault();
-    const { value } = event.target.taskText;
-    if(!value) return;
-    const task = document.createElement('div');
-    task.classList.add('task', 'roundBorder');
-    task.addEventListener('click', changeTaskState)
-    task.textContent = value;
-    tasksContainer.prepend(task);
-    event.target.reset();
+const addNewTask = (event) => {
+  event.preventDefault();
+  const { value } = event.target.taskText;
+  if (!value) return;
+  const task = document.createElement("div");
+  task.classList.add("task", "roundBorder");
+  task.addEventListener("click", changeTaskState);
+  task.textContent = value;
+  tasksContainer.prepend(task);
+  event.target.reset();
 };
 
-const changeTaskState = event => {
-    event.target.classList.toggle('done');
+const changeTaskState = (event) => {
+  event.target.classList.toggle("done");
 };
 
 const order = () => {
-    const done = [];
-    const toDo = [];
-    tasksContainer.childNodes.forEach( el => {
-        el.classList.contains('done') ? done.push(el) : toDo.push(el)
-    })
-    return [...toDo, ...done];
-}
+  const done = [];
+  const toDo = [];
+  tasksContainer.childNodes.forEach((el) => {
+    el.classList.contains("done") ? done.push(el) : toDo.push(el);
+  });
+  return [...toDo, ...done];
+};
 
 const renderOrderedTasks = () => {
-    order().forEach(el => tasksContainer.appendChild(el))
-}
+  order().forEach((el) => tasksContainer.appendChild(el));
+};
 
-const vaciarLista = () =>{
-    
-}
+const vaciarLista = () => {};
 
-<script>
-function limpia(elemento)
-{
-elemento.value = "";
+function limpia(elemento) {
+  elemento.value = "";
 }
-
-function verifica(elemento)
-{
-if(elemento.value = "")
-elemento.value = "Default Value";
-}
-</script>
-
-setDate();
