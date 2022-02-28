b7a98b6696d1535acfa68fcd21fc61ca37236b1f
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-02-28 12:22:18 +0100

    .

---
+++ to-do-main/task.js
@@ -0,0 +1,13 @@
+class task {
+  name;
+  descripcion;
+  status;
+  priority;
+
+  constructor(name, descripcion, status, priority) {
+    this.name = name;
+    this.descripcion = descripcion;
+    this.status = status;
+    this.priority = priority;
+  }
+}
