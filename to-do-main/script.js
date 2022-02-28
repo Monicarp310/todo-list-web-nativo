ed7a7ad52605e9d8fee1d8ed863ea2527c7e8ad9
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-02-28 13:00:46 +0100

    .

--- to-do-main/script.js
+++ to-do-main/script.js
@@ -43,4 +43,16 @@ function limpia(elemento) {
   elemento.value = "";
 }
 
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
+
 document.getElementById("")
